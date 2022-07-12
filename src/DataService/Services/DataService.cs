04c4acca4eb9494ff214dbd79784d242e7bef50a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-07-12 12:11:39 +0100

    Enabled AUtohistory on Dataservice

--- src/DataService/Services/DataService.cs
+++ src/DataService/Services/DataService.cs
@@ -27,7 +27,7 @@ public class DataService : IDataService
         {
             var entity = _mapper.Map<TEntity>(domain);
             var created = _unitOfWork.GetRepository<TEntity>().Insert(entity);
-            await _unitOfWork.CommitAsync();
+            await _unitOfWork.CommitAsync(autoHistory: true);
             return new SingleResponse<TResponse>(_mapper.Map<TResponse>(created));
         }
         catch (DbUpdateException e)
@@ -61,7 +61,7 @@ public class DataService : IDataService
             var patched = _mapper.Map(mapped, entity);
 
             _unitOfWork.GetRepository<TEntity>().Update(patched);
-            await _unitOfWork.CommitAsync();
+            await _unitOfWork.CommitAsync(autoHistory: true);
             var updated = _mapper.Map<TResponse>(patched);
             return new SingleResponse<TResponse>(updated);
         }
@@ -84,7 +84,7 @@ public class DataService : IDataService
             var entity = await _unitOfWork.GetRepositoryAsync<TEntity>().SingleOrDefaultAsync(predicate, enableTracking: true);
             var updated = _mapper.Map(domain, entity);
             _unitOfWork.GetRepository<TEntity>().Update(updated);
-            await _unitOfWork.CommitAsync();
+            await _unitOfWork.CommitAsync(autoHistory: true);
             var result = _mapper.Map<TResponse>(updated);
             return new SingleResponse<TResponse>(result);
         }
